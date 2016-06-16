c78ded61682e3117ec6fd2d08fff932303faeb31
Benjamin Wilkins <ben.wilkins@airbnb.com>
2016-06-15 18:27:20 -0700

    Setup boilerplate

---
+++ components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // TODO: Setup
+  }
+
+  componentWillUnmount() {
+    // TODO: Teardown
+  }
+
+  render() {
+    return (
+      <div>
+        React Webpack Boilerplate.
+      </div>
+    );
+  }
+}
