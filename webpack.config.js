01a751af865bedca98aeb3497023dafa50605fc5
Benjamin Wilkins <ben.wilkins@airbnb.com>
2016-06-19 20:59:08 -0700

    Update webpack to 2.0 beta

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel",
-      include: __dirname,
-      query: {
-        presets: [ 'es2015', 'react' ]
-      }
-    }]
-  },
-  entry: [
-    './index.js'
-  ],
-  output: {
-    path: 'public',
-    filename: 'main.bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'App',
-      template: 'index.ejs',
-    }),
-  ],
+module.exports = (env) => {
+  return {
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        include: __dirname,
+        query: {
+          presets: [ 'es2015', 'react' ]
+        }
+      }]
+    },
+    entry: [
+      './index.js'
+    ],
+    output: {
+      path: 'public',
+      filename: 'main.bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'App',
+        template: 'index.ejs',
+      }),
+    ],
+  };
 };
